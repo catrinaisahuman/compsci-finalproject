002f40d2d37879e1cd9ddf287e80cdc09365c465
catrinaisahuman <jimmydababby@gmail.com>
2019-05-21 14:56:19 -0700

    added data processing script

---
+++ FinalProject/neural-nets/src/dataProcessing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def process(inputData):
+	data = np.genfromtxt(inputData, skip_header=1, delimiter=',')
+	data = [data]
+	data = np.transpose(data)
+	data = ([data], 3)
+	return data
